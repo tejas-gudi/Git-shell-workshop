b9f250500316e4dd1cabd9e1125491348e1e2202
Anushkaghei <anushkaghei@gmail.com>
2023-09-02 11:58:47 +0530

    Task1_done

--- script.sh
+++ script.sh
@@ -5,3 +5,31 @@
 # Run the python file, using input1.txt as stdin, and write the output to a file user_output1.txt 
 # Display user_output1.txt to the user as user output, and the file output1.txt as the expected output 
 # Once you have tested out the script and verified it's working, commit it to a branch: <your_name>
+
+#Task 1
+language=$1
+echo "The language passed as argument is $language"
+cat input1.txt | python3 $2 > user_output1.txt
+echo "User output:"
+cat user_output1.txt
+echo "Expected output:"
+cat output1.txt
+
+
+#Task 2
+for var in "$@"
+do 
+	echo "Running $var on test cases"
+	for i in $(seq 3)
+	do 
+		echo "Running test case $i:"
+		cat input$i.txt | Python3 $var>user_output$i.txt 
+		echo "User output: "
+		cat user_output$i.txt
+		echo "Expected output:"
+		cat output$i.txt
+		echo "--------------------------------------------------"
+	done
+	echo "================================================"
+done
+
